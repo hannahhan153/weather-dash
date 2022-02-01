0e23bcfb4453729ec869d6980e25cabd3169a805
hannahhan153 <hannah.han@biola.edu>
2022-01-31 22:40:14 -0800

    variables for js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function startApp() {
     var searchEl = document.getElementById("search-button");
     var cityEl = document.getElementById("chosen-city");
     var clearEl = document.getElementById("clear-history");
+    var nameEl = document.getElementById("city-name");
+    var iconEl = document.getElementById("icon");
+    var tempEl = document.getElementById("temperature");
+    var humidityEl = document.getElementById("humidity");
+    var windEl = document.getElementById("wind-speed");
+    var uvEl = document.getElementById("uv-index");
 }
 
 startApp();
