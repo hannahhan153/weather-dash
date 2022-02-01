b04e04fb2bbf4392b87a7e27fd620c7de66c2d4b
hannahhan153 <hannah.han@biola.edu>
2022-01-31 22:06:13 -0800

    search elements on js

---
+++ script.js
@@ -0,0 +1,8 @@
+function startApp() {
+    // elements that are part of the search bar
+    var searchEl = document.getElementById("search-button");
+    var cityEl = document.getElementById("city-name");
+    var clearEl = document.getElementById("clear-history");
+}
+
+startApp();
