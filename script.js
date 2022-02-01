9282b7f10a8e603dcceb1e67ba84d80ddcec8d49
hannahhan153 <hannah.han@biola.edu>
2022-01-31 22:35:53 -0800

    local weather

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function startApp() {
     // elements that are part of the search bar
     var searchEl = document.getElementById("search-button");
-    var cityEl = document.getElementById("city-name");
+    var cityEl = document.getElementById("chosen-city");
     var clearEl = document.getElementById("clear-history");
 }
 
