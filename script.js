62b8a772435cbf7a11266c45fe40bb64a5a226be
hannahhan153 <hannah.han@biola.edu>
2022-02-03 21:27:13 -0800

    weather api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function startApp() {
+
     // elements that are part of the search bar
     var searchEl = document.getElementById("search-button");
     var cityEl = document.getElementById("chosen-city");
@@ -9,6 +9,24 @@ function startApp() {
     var humidityEl = document.getElementById("humidity");
     var windEl = document.getElementById("wind-speed");
     var uvEl = document.getElementById("uv-index");
-}
+    const apiKey = "486c5ee4ab0f079c93dc3b5e85a913c2";
+
+
+searchEl.addEventListener("click", function() {
+    
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityEl.value}&units=imperial&appid=${apiKey}`).then(function (response) {
+        console.log(response)
+    
+        return response.json()
+    }).then(function (data){
+        console.log(data)
+        console.log(data.list[0].main.temp)
+    })
+})
+
+
+// API key 
+
+
 
 startApp();
